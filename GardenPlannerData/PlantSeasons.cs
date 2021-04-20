be1c6729d3c61993b3c3654dd6f1e916ef493899
aschump <schumpertamber@gmail.com>
2021-04-20 10:23:31 -0400

    Added PlantSeasons Table

---
+++ GardenPlannerData/PlantSeasons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenPlannerData
+{
+    public class PlantSeasons
+    {
+        [Key]
+        public int SeasonID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; }
+    }
+}

7b40e24782abda1d3d9e0e649eb9e6870986e642
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-04-20 09:38:26 -0400

    Created MyPlant model

---
+++ GardenPlannerModels/MyPlant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenPlannerModels
+{
+    public class MyPlant
+    {
+        public string Location { get; set; }
+        public string PlantName { get; set; }
+        public DateTimeOffset DatePlanted { get; set; }
+        public string Notes { get; set; }
+        public int Year { get; set; }
+        public string Photo { get; set; }
+    }
+}

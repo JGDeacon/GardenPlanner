18ce1042086b289e64b77868bdbbe6d2371d5640
aschump <schumpertamber@gmail.com>
2021-04-20 14:00:24 -0400

    Added PlantCare Model and updated MyPlants

---
+++ GardenPlannerModels/PlantCare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenPlannerModels
+{
+    public class PlantCare
+    {
+        public int SunExposureID { get; set; }
+        public int WaterNeedID { get; set; }
+        public string Temperature { get; set; }
+
+        [MaxLength(3000)]
+        public string Description { get; set; }
+    }
+}

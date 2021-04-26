9eb730473661747215a6a09cde5badae12c95883
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-04-26 10:59:40 -0400

    Creating Models

---
+++ GardenPlannerModels/CommentModels/AddCommentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenPlannerModels.CommentModels
+{
+    public class AddCommentModel
+    {
+        public int PlantID { get; set; }
+        [MaxLength(50, ErrorMessage ="50 char limit")]
+        public string Title { get; set; }
+        [MaxLength(3000, ErrorMessage ="3000 char limit")]
+        public string Comment { get; set; }
+    }
+}

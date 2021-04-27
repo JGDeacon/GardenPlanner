c6fa2db6b18dd0dc30f5b854fffbd47c3a1b6f14
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-04-27 11:18:25 -0400

    Seed items

---
+++ GardenPlannerAPI/Controllers/SeedDataController.cs
@@ -0,0 +1,20 @@
+﻿using GardenPlannerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GardenPlannerAPI.Controllers
+{
+    public class SeedDataController : ApiController
+    {
+        public IHttpActionResult Post()
+        {
+            var service = new SeedService();
+            service.SeedPlants();
+            return Ok("Plants Seeded");
+        }
+    }
+}

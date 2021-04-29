85e99b44a61c105f8266d8314ddfc3478cf15120
ShirishaB88 <shirisha46@gmail.com>
2021-04-29 09:34:19 -0400

    Changes in UpdatePlantModel and updateplantmethod in plantservice

--- GardenPlannerModels/UpdatePlantModel.cs
+++ GardenPlannerModels/UpdatePlantModel.cs
@@ -14,12 +14,11 @@ namespace GardenPlannerModels
         public int ZoneID { get; set; }
         public int SeasonID { get; set; }
         public int PlantTypeID { get; set; }
-        public int PlantCareID { get; set; }
         public int SunExposureID { get; set; }
         public int WaterNeedID { get; set; }
         public string Temperature { get; set; }
         [MaxLength(3000)]
         public string Description { get; set; }
-        public int PlantDetailsID { get; set; }
+       
     }
 }
